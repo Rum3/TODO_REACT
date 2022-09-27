229f6b1787ddb60bbb5e4b8d5ec43ae2f12388d0
Rum3 <rum3n1992@gmail.com>
2022-09-27 10:57:55 +0300

    Json server

--- src/App.js
+++ src/App.js
@@ -3,59 +3,102 @@ import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
 
-const LOCAL_STORAGE_KEY = "todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  let url = 'http://localhost:5000/todos';
 
-  useEffect(() => {
-    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storageTodos) {
-      setTodos(storageTodos);
-    }
-  }, []);
+    
 
-  useEffect(() => {
-   
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
+  const fetchTodos = ()=>{
+  fetch(url)
+    .then(r=>{
+      if(r.ok){
+        return r.json()
+      }
+    })
+    .then(data=> {
+      setTodos(data)
+    })
+    .catch( err=>console.warn(err) );
+  }
+
+
 
   const addTodo = (text) => {
-    let id = 1;
-    if(todos.length > 0) {
-      id = todos[0].id + 1
+    let todo = {
+      id: new Date().getTime().toString(), 
+      text: text, 
+      completed: false, 
     }
-    let todo = {id: id, text: text, completed: false, important: false}
-    let newTodos = [todo, ...todos]
-    setTodos(newTodos)
+
+    fetch(url, {
+      method:"Post",
+      body:JSON.stringify(todo),
+      headers:{
+          "content-type":"application/json"
+      }
+  })
+  .then(res=>{
+      if(res.ok){
+          return res.json()
+      }
+  })
+  .then(todo=>{
+      setTodos([...todos,todo])
+  })    
+  
   };
 
   const removeTodo = (id) => {
-    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+    let updatedTodos = todos.filter((todo) => todo.id !== id);
+    console.log(id);
+
+    fetch(url+"/"+id, {
+      method:"Delete",
+      body:JSON.stringify(updatedTodos),
+      headers:{
+          "content-type":"application/json"
+      }
+  })
+  .then(res=>{
+      if(res.ok){
+          return res.json()
+      }
+  })
+  .then(todo=>{
+      setTodos([...updatedTodos])
+  })    
+   
   };
 
   const completeTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
+    let updatedTodos = todos.filter((todo) => todo.id === id)[0]
+   
+    fetch(url+"/"+id, {
+      method:"PATCH",
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        completed:!updatedTodos.completed,
+      }),
+  })
+  .then(res=>{
+      if(res.ok){
+          return res.json()
       }
-      return todo
-    })
-    setTodos(updatedTodos)
+  })
+  .then(data=>{
+    setTodos(todos.map(todo=>todo.id===id?data:todo));
+  })    
   }
 
-  const importantTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
-      if(todo.id === id) {
-        todo.important = !todo.important
-      }
-      return todo
-    })
+ 
 
-    setTodos(updatedTodos)
-  }
+
   let sortedTodos = todos.sort((a, b) => b.important - a.important)
+
+  useEffect(fetchTodos,[])
+
   return (
     <div className="todo-app">
       <h1>Todo List</h1>
@@ -63,7 +106,7 @@ function App() {
       <hr className="seperator"/>
       {sortedTodos.map((todo) => {
         return (
-          <TodoItem removeTodo={removeTodo} completeTodo={completeTodo} importantTodo={importantTodo} todo={todo} key={todo.id}/>
+          <TodoItem removeTodo={removeTodo} completeTodo={completeTodo} todo={todo} key={todo.id}/>
         )
       })}
     </div>
